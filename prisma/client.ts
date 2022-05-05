202b7791e105ad2a6c88ba1dd44bab65c10a1071
pajecawav <pajecawav@gmail.com>
2022-05-05 20:03:02 +0300

    fix database url env variable hack

--- prisma/client.ts
+++ prisma/client.ts
@@ -6,17 +6,7 @@ declare global {
 	var db: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
-
-// HACK: This module is imported during graphql types generation in CI where no
-// `DATABASE_URL` env variable is specified so the prisma client throws an
-// error and stops generation script from working. To avoid this we detect CI
-// environment and avoid instantiating PrismaClient in those.
-if (isCi) {
-	prisma = null as any as PrismaClient;
-} else {
-	prisma = global.db || new PrismaClient();
-}
+const prisma = global.db || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
 	global.db = prisma;
